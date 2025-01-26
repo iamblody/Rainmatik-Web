c5ed28aa98929c190ef9193eb28168f64cadc881
blody <128133944+iamblody@users.noreply.github.com>
2025-01-27 00:18:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// Sayaç değerini tutan değişken
+let count = 0;
+
+// HTML elemanlarını seç
+const countDisplay = document.getElementById("count");
+const increaseBtn = document.getElementById("increase-btn");
+const resetBtn = document.getElementById("reset-btn");
+const eksiBtn = document.getElementById("eksi-btn");
+
+// Zikir sayısını artıran işlev
+increaseBtn.addEventListener("click", () => {
+  count++;
+  countDisplay.textContent = count;
+});
+
+eksiBtn.addEventListener("click", () => {
+    count--;
+    countDisplay.textContent = count;
+})
+
+// Zikir sayısını sıfırlayan işlev
+resetBtn.addEventListener("click", () => {
+  count = 0;
+  countDisplay.textContent = count;
+});
